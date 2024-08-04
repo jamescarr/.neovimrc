5d760419668bb81a62256ae03300c99c0a62c9d5
James Carr <james.r.carr@gmail.com>
2024-08-04 07:02:36 -0500

    Add autotag plugin

---
+++ lua/jamescarr/lazy/autotag.lua
@@ -0,0 +1,23 @@
+return {
+    "windwp/nvim-ts-autotag",
+    config = function()
+        local autotag = require("windwp/nvim-ts-autotag")
+        autotag.setup({
+        opts = {
+            -- Defaults
+            enable_close = true, -- Auto close tags
+            enable_rename = true, -- Auto rename pairs of tags
+            enable_close_on_slash = false -- Auto close on trailing </
+          },
+         per_filetype = {
+            ["html"] = {
+                enable_close = true
+            }
+        },
+        aliases = {
+            ["heex"] = "html"
+        }
+    })
+
+    end
+}
