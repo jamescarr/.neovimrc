39c3f899961f6a9f0539c8c4e62de419aec5c488
James Carr <james.r.carr@gmail.com>
2024-12-20 11:46:24 -0600

    Cleanup fugitive file layout

--- lua/jamescarr/lazy/fugitive.lua
+++ lua/jamescarr/lazy/fugitive.lua
@@ -1,49 +1,54 @@
+-- Global GitLab domain configuration
+vim.g.fugitive_gitlab_domains = { "https://gitlab.com" }
 
-vim.g.fugitive_gitlab_domains = {'https://gitlab.com'}
-return {
-    "tpope/vim-fugitive",
-    {
-    -- Add the fugitive-gitlab plugin
-        "shumphrey/fugitive-gitlab.vim",
-        config = function()
-            -- Configure the keymap for GBrowse
-            vim.keymap.set("n", "<leader>gb", vim.cmd.GBrowse)
+-- Define the Fugitive configuration module
+local function fugitive_config()
+    -- General Fugitive keymaps
+    vim.keymap.set("n", "<leader>gs", vim.cmd.Git) -- Git status
+    vim.keymap.set("n", "gu", "<cmd>diffget //2<CR>") -- Git diffget left
+    vim.keymap.set("n", "gh", "<cmd>diffget //3<CR>") -- Git diffget right
+
+    -- Augroup for Fugitive-specific commands
+    local fugitive_augroup = vim.api.nvim_create_augroup("FugitiveConfig", { clear = true })
+
+    vim.api.nvim_create_autocmd("BufWinEnter", {
+        group = fugitive_augroup,
+        pattern = "*",
+        callback = function()
+            if vim.bo.filetype ~= "fugitive" then
+                return
+            end
+
+            local bufnr = vim.api.nvim_get_current_buf()
+            local opts = { buffer = bufnr, remap = false }
+
+            -- Fugitive-specific keymaps
+            vim.keymap.set("n", "<leader>p", function()
+                vim.cmd.Git("push")
+            end, opts)
+
+            vim.keymap.set("n", "<leader>P", function()
+                vim.cmd.Git({ "pull", "--rebase" })
+            end, opts)
+
+            vim.keymap.set("n", "<leader>t", ":Git push -u origin ", opts) -- Track remote branch
         end,
+    })
+end
+
+-- Define the Fugitive GitLab plugin configuration
+local function gitlab_config()
+    vim.keymap.set("n", "<leader>gb", vim.cmd.GBrowse) -- Open in GitLab
+end
+
+-- Plugin specification for Lazy.nvim
+return {
+    {
+        "tpope/vim-fugitive",
+        config = fugitive_config,
     },
-    config = function()
-
-        vim.keymap.set("n", "<leader>gs", vim.cmd.Git)
-
-        local JamesCarr_Fugitive = vim.api.nvim_create_augroup("JamesCarr_Fugitive", {})
-
-        local autocmd = vim.api.nvim_create_autocmd
-        autocmd("BufWinEnter", {
-            group = JamesCarr_Fugitive,
-            pattern = "*",
-            callback = function()
-                if vim.bo.ft ~= "fugitive" then
-                    return
-                end
-
-                local bufnr = vim.api.nvim_get_current_buf()
-                local opts = {buffer = bufnr, remap = false}
-                vim.keymap.set("n", "<leader>p", function()
-                    vim.cmd.Git('push')
-                end, opts)
-
-                -- rebase always
-                vim.keymap.set("n", "<leader>P", function()
-                    vim.cmd.Git({'pull',  '--rebase'})
-                end, opts)
-
-                -- NOTE: It allows me to easily set the branch i am pushing and any tracking
-                -- needed if i did not set the branch up correctly
-                vim.keymap.set("n", "<leader>t", ":Git push -u origin ", opts);
-            end,
-        })
-
-
-        vim.keymap.set("n", "gu", "<cmd>diffget //2<CR>")
-        vim.keymap.set("n", "gh", "<cmd>diffget //3<CR>")
-    end
+    {
+        "shumphrey/fugitive-gitlab.vim",
+        config = gitlab_config,
+    },
 }
